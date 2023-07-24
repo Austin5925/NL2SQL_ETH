110e0bec9145aabac875879e5b66a652ef296222
Austin5925 <z1026804039@163.com>
2023-07-24 18:14:44 +0800

    v0.1 MVP

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# import asyncio
+
 # import datetime
 #
 # import pprint
@@ -8,8 +8,9 @@
 import json
 import os
 from collections import defaultdict
-from typing import AsyncIterable, List, Optional
+from typing import AsyncGenerator, List, Optional
 
+import redis
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -58,7 +59,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-async def request(val: List[dict[str, str]]) -> AsyncIterable[str]:
+async def request(val: List[dict[str, str]]) -> AsyncGenerator[dict, None]:
     url = api_url
     headers = {
         "Content-Type": "application/json",
@@ -68,14 +69,14 @@ async def request(val: List[dict[str, str]]) -> AsyncIterable[str]:
         "model": "gpt-3.5-turbo",
         "messages": val,
         "max_tokens": 2000,
-        "temperature": 0.8,
+        "temperature": 0.5,
         "top_p": 1,
         "n": 1,
         "stream": True,
     }
     async with AsyncClient() as client:
         async with client.stream(
-            "POST", url, headers=headers, json=params, timeout=60
+                "POST", url, headers=headers, json=params, timeout=60
         ) as response:
             async for line in response.aiter_lines():
                 if not line.strip():
@@ -95,16 +96,28 @@ async def request(val: List[dict[str, str]]) -> AsyncIterable[str]:
                 # pprint.pprint(response.json())
 
 
-async def draw_chart(sql: str) -> dict:
-    """绘制图表"""
+def get_sql_execute_result(sql: str) -> dict:
+    """
+    得到SQL查询结果
+    """
     db = dbtool.DatabaseManager()
-    result = await db.execute_query(input)
-    await db.close()
+    result = db.execute_query(sql)
+    db.close()
     return result
+
+
+def draw_charts(sql_result: dict) -> None:
+    """
+    画图
+    """
+    pass
 
 
 @app.get("/chat")
 async def chat_stream(input: Optional[str] = None) -> EventSourceResponse:
+    """
+    查询入口
+    """
     question = input
 
     base_prompt = f"""
@@ -125,7 +138,8 @@ async def chat_stream(input: Optional[str] = None) -> EventSourceResponse:
         - `TokenSymbol`：varchar(32)
         用户的问题是：{question}
         请注意：
-        1. 你的回答只需要包含SQL语句。
+        1.你的回答必须只能包含SQL语句，不要有其他废话。
+        2.回答的SQL语句以分号结尾。
     """
 
     message = [{"role": "user", "content": base_prompt}]
@@ -133,19 +147,63 @@ async def chat_stream(input: Optional[str] = None) -> EventSourceResponse:
     print(chat_msg)
 
     async def event_generator():
-        async for delta in request(message):
-            if delta.get("role"):
-                chat_msg["role"] = delta.get("role")
-            if delta.get("content"):
-                print(delta.get("content"))
-                chat_msg["content"] += delta.get("content")
+        """生成事件，获取流信息"""
+        try:
+            async for delta in request(message):
+                if delta.get("role"):
+                    chat_msg["role"] = delta.get("role")
+                if delta.get("content"):
+                    chat_msg["content"] += delta.get("content")
                 yield dict(id=None, event=None, data=json.dumps(chat_msg))
-                # await asyncio.sleep(0.1)
+        except Exception as e:
+            print("error: ", e)
 
-    result = chat_msg["content"]
-    draw_chart(result)
+        sql_result = chat_msg["content"]
+        print(sql_result)
+        executed_result_dict = get_sql_execute_result(sql_result)
+        print(executed_result_dict)
+        if executed_result_dict["code"] == 200:
+            executed_result = executed_result_dict["data"]
+            draw_charts(executed_result)
+        else:
+            executed_result = executed_result_dict
+
+        # 将执行结果存入redis，在本async方法中，无法直接返回执行结果，因为返回的是一个异步生成器
+        # 在前端通过ajax轮询redis获取执行结果
+        # 因为对应前端方法，也不适合需要传参的asyncio的Future或者Queue
+        redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+        redis_conn.set('sql_executed_result', json.dumps(executed_result))
+        redis_conn.close()
 
     return EventSourceResponse(event_generator())
+
+
+@app.get("/chat/result")
+def get_executed_result():
+    """
+    前端通过获取SQL执行结果，从redis中返回
+    """
+    redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+    result = redis_conn.get('sql_executed_result')
+    # redis_conn.delete('sql_executed_result')
+    redis_conn.close()
+    if result is not None:
+        result = json.loads(result)
+        print(result)
+        if isinstance(result, dict):
+            return result
+            # 错误
+        else:
+            # 正确结果
+            return JSONResponse(
+                status_code=200,
+                content=result,
+            )
+    else:
+        return JSONResponse(
+            status_code=204,
+            content={"message": "no result"},
+        )
 
 
 @app.exception_handler(Exception)
